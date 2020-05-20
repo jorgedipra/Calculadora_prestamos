9ebfb100dade0502c6add2cb5bbe96aa371d220d
Jorge Diaz <jorgedipra@gmail.com>
2020-05-20 15:21:37 -0500

    conversión de tasa anticipadas a vencidas

--- js/convercion_tasa.js
+++ js/convercion_tasa.js
@@ -31,7 +31,12 @@ function convercion_tasa() {
     if (tasa == 1) { //Vencida
         switch (sub_tasa) {
             case "1": //Mensual
-                interes_salida = interes_entrada;
+                if (interes_entrada > 0) {
+                    interes_salida = interes_entrada;
+                } else {
+                    invalidar('i_entrada');
+                    interes_salida = 0;
+                }
                 break;
             case "2": //Bimestral
                 if (interes_entrada > 0) {
@@ -97,19 +102,87 @@ function convercion_tasa() {
 
     } else { //Anticipada
         switch (sub_tasa) {
-            case 1:
-
+            case "1": //mensual
+                if (interes_entrada > 0) {
+                    //i=ia/1-ia
+                    $cal0 = parseInt(interes_entrada) / 100;
+                    $cal1 = $cal0 / (1 - $cal0);
+                    interes_salida = $cal1 * 100;
+                } else {
+                    invalidar('i_entrada');
+                    interes_salida = 0;
+                }
                 break;
-            case 2:
+            case "2": //Bimestral
+                if (interes_entrada > 0) {
+                    //i=ia/1-ia
+                    $spider0 = parseInt(interes_entrada) / 100;
+                    $spider1 = $spider0 / (1 - $spider0);
+                    //(((1+i)^12)raiz(6))-1
+                    $cal0 = $spider1;
+                    $cal1 = 1 + $cal0;
+                    $cal2 = Math.pow($cal1, 12);
+                    $cal3 = Math.pow($cal2, 1 / 6);
+                    $cal = $cal3 - 1;
 
+                    interes_salida = $cal * 100;
+                } else {
+                    invalidar('i_entrada');
+                    interes_salida = 0;
+                }
                 break;
-            case 3:
+            case "3": //Trimestral
+                if (interes_entrada > 0) {
+                    //i=ia/1-ia
+                    $spider0 = parseInt(interes_entrada) / 100;
+                    $spider1 = $spider0 / (1 - $spider0);
+                    //(((1+i)^12)raiz(4))-1
+                    $cal0 = $spider1;
+                    $cal1 = 1 + $cal0;
+                    $cal2 = Math.pow($cal1, 12);
+                    $cal3 = Math.pow($cal2, 1 / 4);
+                    $cal = $cal3 - 1;
 
+                    interes_salida = $cal * 100;
+                } else {
+                    invalidar('i_entrada');
+                    interes_salida = 0;
+                }
                 break;
-            case 4:
+            case "4": //Semestral
+                if (interes_entrada > 0) {
+                    //i=ia/1-ia
+                    $spider0 = parseInt(interes_entrada) / 100;
+                    $spider1 = $spider0 / (1 - $spider0);
+                    //(((1+i)^12)raiz(2))-1
+                    $cal0 = $spider1;
+                    $cal1 = 1 + $cal0;
+                    $cal2 = Math.pow($cal1, 12);
+                    $cal3 = Math.pow($cal2, 0.5);
+                    $cal = $cal3 - 1;
 
+                    interes_salida = $cal * 100;
+                } else {
+                    invalidar('i_entrada');
+                    interes_salida = 0;
+                }
                 break;
-            case 5:
+            case "5": //Anual
+                if (interes_entrada > 0) {
+                    //i=ia/1-ia
+                    $spider0 = parseInt(interes_entrada) / 100;
+                    $spider1 = $spider0 / (1 - $spider0);
+                    //((1+i)^12)-1
+                    $cal0 = $spider1;
+                    $cal1 = 1 + $cal0;
+                    $cal2 = Math.pow($cal1, 12);
+                    $cal = $cal2 - 1;
+
+                    interes_salida = $cal * 100;
+                } else {
+                    invalidar('i_entrada');
+                    interes_salida = 0;
+                }
 
                 break;
         } //::END=>switch
